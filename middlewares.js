caf76b90c782d1bdc35cde7f6e1ddef921959f3f
YongJaeKim <kiyoja07@gmail.com>
2020-11-10 14:04:51 +0900

    header and footer layout --ing

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,7 @@
 import routes from "./routes";
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "SolTone";
+    res.locals.siteName = "솔톤 세무";
     res.locals.routes = routes;
     next();
   };
