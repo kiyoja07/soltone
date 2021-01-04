3d7da5f4b78c1e62c3de6ac434d2bf7fd9652eea
YongJaeKim <kiyoja07@gmail.com>
2021-01-04 16:59:35 +0900

    canonical url

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import routes from "../routes";
 
 dotenv.config();
 
@@ -8,19 +9,19 @@ const mapApiKey = process.env.MAP_API_KEY;
 // Home
 export const home = (req, res) => {
     try {
-      res.render("home", { pageTitle: "Home", mapAppKey, mapApiKey });
+      res.render("home", { pageTitle: "Home", canonicalUrl: routes.home, mapAppKey, mapApiKey });
     } catch (error) {
       console.log(`res.render("home") error : ${error}`);
-      res.render("home", { pageTitle: "Home", mapAppKey: [], mapApiKey: [] });
+      res.render("home", { pageTitle: "Home", canonicalUrl: routes.home, mapAppKey: [], mapApiKey: [] });
     }
 };
 
 // People
 export const people = (req, res) => {
   try {
-    res.render("people", { pageTitle: "People" });
+    res.render("people", { pageTitle: "People", canonicalUrl: routes.people });
   } catch (error) {
     console.log(`res.render("home") error : ${error}`);
-    res.render("people", { pageTitle: "People" });
+    res.render("people", { pageTitle: "People", canonicalUrl: routes.people });
   }
 };
