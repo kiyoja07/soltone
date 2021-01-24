e995787504d4b08414cc622cab34a4ef19470c2d
YongJaeKim <kiyoja07@gmail.com>
2021-01-24 19:01:14 +0900

    add analytics script

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -6,22 +6,24 @@ dotenv.config();
 const mapAppKey = process.env.MAP_APP_KEY;
 const kakaoMapApi = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${mapAppKey}&libraries=services,clusterer,drawing`
 
+const NaverAnalyticsId = process.env.NAVER_ANALYTICS_ID;
+
 // Home
 export const home = (req, res) => {
     try {
-      res.render("home", { pageTitle: "Home", canonicalUrl: routes.home, kakaoMapApi });
+      res.render("home", { pageTitle: "Home", canonicalUrl: routes.home, NaverAnalyticsId, kakaoMapApi });
     } catch (error) {
       console.log(`res.render("home") error : ${error}`);
-      res.render("home", { pageTitle: "Home", canonicalUrl: routes.home, kakaoMapApi: [] });
+      res.render("home", { pageTitle: "Home", canonicalUrl: routes.home, NaverAnalyticsId, kakaoMapApi: [] });
     }
 };
 
 // People
 export const people = (req, res) => {
   try {
-    res.render("people", { pageTitle: "People", canonicalUrl: routes.people, kakaoMapApi: [] });
+    res.render("people", { pageTitle: "People", canonicalUrl: routes.people, NaverAnalyticsId, kakaoMapApi: [] });
   } catch (error) {
     console.log(`res.render("home") error : ${error}`);
-    res.render("people", { pageTitle: "People", canonicalUrl: routes.people });
+    res.render("people", { pageTitle: "People", canonicalUrl: routes.people, NaverAnalyticsId });
   }
 };
