c46a624dce8aedec0b32f5eb2416f1c0aed7fc68
YongJaeKim <kiyoja07@gmail.com>
2021-02-14 17:49:33 +0900

    mini-css-extract-plugin

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
+const MiniExtractCSS = require("mini-css-extract-plugin");
 const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
-const OUTPUT_DIR = path.join(__dirname, "static");
+const OUTPUT_DIR = path.resolve(__dirname, "static");
 
 const config = {
   entry: ["@babel/polyfill", ENTRY_FILE],
@@ -20,25 +20,30 @@ const config = {
         ],
       },
       {
-        test: /\.(scss)$/,
-        use: ExtractCSS.extract([
+        test: /\.scss$/,
+        use: [
           {
-            loader: "css-loader",
+            loader: MiniExtractCSS.loader,
+            options: {
+              hmr: process.env.WEBPACK_ENV === "development",
+            },
           },
+          "css-loader",
           {
             loader: "postcss-loader",
             options: {
-              postcssOptions: {
-                plugins() {
-                  return [autoprefixer({ browsers: "cover 99.5%" })];
-                },
+              plugins() {
+                return [
+                  autoprefixer({
+                    overrideBrowserslist: "cover 99.5%",
+                  }),
+                ];
               },
             },
           },
-          {
-            loader: "sass-loader",
-          },
-        ]),
+
+          "sass-loader",
+        ],
       },
     ],
   },
@@ -46,7 +51,66 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js",
   },
-  plugins: [new ExtractCSS("styles.css")],
+  plugins: [
+    new MiniExtractCSS({
+      filename: "styles.css",
+    }),
+  ],
 };
 
 module.exports = config;
+
+
+
+// const path = require("path");
+// const autoprefixer = require("autoprefixer");
+// const ExtractCSS = require("extract-text-webpack-plugin");
+
+// const MODE = process.env.WEBPACK_ENV;
+// const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+// const OUTPUT_DIR = path.join(__dirname, "static");
+
+// const config = {
+//   entry: ["@babel/polyfill", ENTRY_FILE],
+//   mode: MODE,
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js)$/,
+//         use: [
+//           {
+//             loader: "babel-loader",
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(scss)$/,
+//         use: ExtractCSS.extract([
+//           {
+//             loader: "css-loader",
+//           },
+//           {
+//             loader: "postcss-loader",
+//             options: {
+//               postcssOptions: {
+//                 plugins() {
+//                   return [autoprefixer({ browsers: "cover 99.5%" })];
+//                 },
+//               },
+//             },
+//           },
+//           {
+//             loader: "sass-loader",
+//           },
+//         ]),
+//       },
+//     ],
+//   },
+//   output: {
+//     path: OUTPUT_DIR,
+//     filename: "[name].js",
+//   },
+//   plugins: [new ExtractCSS("styles.css")],
+// };
+
+// module.exports = config;
