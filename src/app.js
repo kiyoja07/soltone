310e1104b2fb6f3b8cc83c8bfe28fbb90b469ab5
YongJaeKim <kiyoja07@gmail.com>
2021-04-01 17:37:31 +0900

    add favicon.ico

--- src/app.js
+++ src/app.js
@@ -1,34 +1,35 @@
-import express from 'express';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { localsMiddleware } from './middlewares';
-import { generateSitemap } from './sitemap';
-import routes from './routes';
-import globalRouter from './routers/globalRouter';
-import blogRouter from './routers/blogRouter';
+import express from "express";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import path from "path";
+import { localsMiddleware } from "./middlewares";
+import { generateSitemap } from "./sitemap";
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
+import blogRouter from "./routers/blogRouter";
+import favicon from "serve-favicon";
 
 const app = express();
 
 app.use(helmet({ contentSecurityPolicy: false }));
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 // app.use("/uploads", express.static("uploads"));
 // app.use("/static", express.static("static"));
-app.set('views', path.join(__dirname, 'views'));
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(localsMiddleware);
 
-app.get('/robots.txt', (req, res) => {
-  res.type('text/plain');
-  res.send(
-    'User-agent: *\nAllow: /\nDisallow: /admin/\n',
-  );
+app.get("/robots.txt", (req, res) => {
+  res.type("text/plain");
+  res.send("User-agent: *\nAllow: /\nDisallow: /admin/\n");
 });
+
+app.use(favicon(path.join(__dirname, "public/images", "favicon.ico")));
 
 // app.get(routes.sitemap, (req, res) => {
 //   res.sendFile('/sitemap.xml', { root: __dirname });
